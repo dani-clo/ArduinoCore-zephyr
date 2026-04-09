f9f525506a4416643a2c0897e2ebd89999cdc5a6
Daniele Cloralio <d.cloralio@arduino.cc>
2026-04-09 12:41:30 +0200

    opta: rs485 serial define

--- variants/arduino_opta_stm32h747xx_m7/variant.h
+++ variants/arduino_opta_stm32h747xx_m7/variant.h
@@ -5,3 +5,10 @@
  */
 
 #pragma once
+
+/* RS485 configuration for ArduinoModbus/ArduinoRS485 libraries on USART3 (Serial1) */
+#define RS485_SERIAL_PORT           Serial1
+
+#define RS485_DEFAULT_TX_PIN        -1             // TX managed by UART hardware
+#define RS485_DEFAULT_DE_PIN        21             // D21 = gpiob 14 (Driver Enable)
+#define RS485_DEFAULT_RE_PIN        22             // D22 = gpiob 13 (Receiver Enable)
